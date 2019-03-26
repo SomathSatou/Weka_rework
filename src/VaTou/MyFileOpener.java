8b27cfc531281f5d1dbe079e8e17303a36a6fb8c
SomathSatou <tomsaout@wanadoo.fr>
2019-03-26 13:23:08 +0100

    project clear

--- src/VaTou/MyFileOpener.java
+++ src/VaTou/MyFileOpener.java
@@ -1,5 +1,25 @@
 package VaTou;
 
-public class MyFileOpener {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+import javax.swing.JFileChooser;
+
+public class MyFileOpener {
+    JFileChooser file_chooser = new JFileChooser();
+
+    public String pick_me() throws FileNotFoundException {
+        String path = "";
+        if ( file_chooser.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION ) {
+            // get the file
+            File file = file_chooser.getSelectedFile();
+            // read the path of file
+            // System.out.println("selected file:"+file.getAbsolutePath() );
+            path = file.getAbsolutePath();
+            System.out.println( path );
+            // ajouter la méthode reset
+
+        }
+        return path;
+    }
 }
